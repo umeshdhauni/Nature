bf9fe01df63dfb782dac223e43595cd2299ba637
umeshdhauni <umeshdhauni@gmail.com>
2016-12-28 17:58:49 +0530

    first commit

---
+++ Nature/Nature.js
@@ -0,0 +1,21 @@
+
+	function list_bar(){
+		$(".list_bar").addClass("bar");
+		$(".list_remove").addClass("remove");
+		$(".b").addClass("b_translate");
+		$(".aa").addClass("b_translate");
+		$(".c").addClass("b_translate");
+		$(".d").addClass("d_translate");
+		$(".e").addClass("e_translate");
+		$(".f").addClass("f_translate");
+	}
+	function list_bar_rm(){
+		$(".list_bar").removeClass("bar");
+		$(".list_remove").removeClass("remove");
+		$(".b").removeClass("b_translate");
+		$(".aa").removeClass("b_translate");
+		$(".c").removeClass("b_translate");
+		$(".d").removeClass("d_translate");
+		$(".e").removeClass("e_translate");
+		$(".f").removeClass("f_translate");
+		}
